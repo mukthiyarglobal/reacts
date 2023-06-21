05919edd7825afabc544dec502bc1601bb893ccf
Karthikeyanmpk786 <129071365+Karthikeyanmpk786@users.noreply.github.com>
2023-06-21 16:43:00 +0530

    test6

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -96,7 +96,7 @@ export default function Admin({ ...rest }) {
     <div className={classes.wrapper}>
       <Sidebar
         routes={routes}
-        logoText={"BG Azure Team"}
+        logoText={"BG Gitlab Team"}
         logo={logo}
         image={image}
         handleDrawerToggle={handleDrawerToggle}
